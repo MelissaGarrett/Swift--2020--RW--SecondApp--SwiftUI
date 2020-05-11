3cb4a5b54bf7e319b899a0550add0057c113b039
MelissaGarrett <melissa_garrett@hotmail.com>
2020-05-11 15:07:23 -0500

    Initial Commit

---
+++ TaskListUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TaskListUI
+//
+//  Created by Melissa  Garrett on 5/11/20.
+//  Copyright © 2020 MelissaGarrett. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
